874cc1c31d6056a3b6b750f1cd83207505616e7c
Theo Vetere <theodore.vetere@gmail.com>
2026-03-02 16:49:09 -0500

    fix auth route for vercel build

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,7 @@
 import { NextAuthOptions } from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import EmailProvider from 'next-auth/providers/email'
-import { prisma } from './db'
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as any,
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER || 'smtp://localhost:1025',
